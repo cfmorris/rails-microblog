ba8aa677f2d04ac4ea1860a9d40bc339b0c82709
Puxton <clifford.morris.0844@gmail.com>
2022-04-09 11:54:40 -0400

    Add function to accept user sign ups

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,8 +16,9 @@ class UsersController < ApplicationController
     end
   end
     
-  priv:after =>  column_name
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
+  private
+    
+    def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
 end
