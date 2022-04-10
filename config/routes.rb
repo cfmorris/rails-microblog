2be26c61879a98c2419c669b0e010496aec7b609
Puxton <clifford.morris.0844@gmail.com>
2022-04-10 15:37:25 -0400

    Fix heroku signup routing

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   get '/contact', to: 'static_pages#contact'
   get '/news', to: 'static_pages#news'
   get '/signup', to: 'users#new'
+  get '/users' , to: 'users#new'
   
   resources :users
   
