baaf0efb1fe28c35ff1ab6311e17d9dea26ec040
Puxton <clifford.morris.0844@gmail.com>
2022-04-10 11:59:00 -0400

    Fix bootstrap import redundancies.

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,8 +1,6 @@
 // Entry point for the build script in your package.json
 import "@hotwired/turbo-rails"
 import "./controllers"
-import "@popperjs/core"
-import "bootstrap"
 
 let popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'))  
 let popoverList = popoverTriggerList.map(function (popoverTriggerEl) {  
