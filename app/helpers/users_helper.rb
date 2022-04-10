8b03a7ad752651832118af921741a7c0963f7f5a
Puxton <clifford.morris.0844@gmail.com>
2022-04-09 21:43:09 -0400

    Add validation feedback to sign up form

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,4 +5,13 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
+
+  def validate_signup(par, user, f)
+    if user.errors.full_messages_for(par).any?
+      f.label par, user.errors.full_messages_for(par).first, style:"color:red;"
+    else
+      f.label par
+    end
+      #f.text_field par      
+  end 
 end
