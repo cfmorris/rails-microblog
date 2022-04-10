2784eeaf975e42fec391e5aade11682eb23fef2f
Puxton <clifford.morris.0844@gmail.com>
2022-04-10 15:01:46 -0400

    Fix heroku signup routing

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,11 +6,11 @@ module UsersHelper
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
 
-  def validated_signup_label(par, user, f)
-    if user.errors.full_messages_for(par).any?
-      f.label par, user.errors.full_messages_for(par).first, style:"color:red;"
+  def validated_signup_label(param, user, f)
+    if user.errors.full_messages_for(param).any?
+      f.label param, user.errors.full_messages_for(param).first, style:"color:red;"
     else
-      f.label par
+      f.label param
     end     
-  end 
+  end
 end
