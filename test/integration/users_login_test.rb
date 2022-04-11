3d4186557d80bc80ad31d75614fa295a9733191f
Puxton <clifford.morris.0844@gmail.com>
2022-04-11 19:19:49 -0400

    Persistent session via DB stores cookies.

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -18,16 +18,24 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert flash.empty?
   end
 
-  test "log in with valid information" do
+  test "log in with valid information followed by logout" do
     get login_path
-    post login_path, params: { session: {email: @user.email,
+    post login_path, params: { session: {email: test@user.email,
                                          password: 'password'} }                                      
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path
+    get logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_select "a[href=?]", logout_path, count: 0
+    assert_select "a[href=?]", user_path, count: 0
+    assert_select "a[href=?]", login_path
     end
 
 
