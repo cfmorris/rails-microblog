a466f63c49a928aef111fbe89619f367fabb3825
Puxton <clifford.morris.0844@gmail.com>
2022-04-11 19:54:04 -0400

    Clean up typos

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,10 +26,10 @@ class User < ApplicationRecord
   end
   
   def authenticated?(remember_token)
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)  
+    BCrypt::Password.create(:remember_token).is_password?(remember_token)  
   end
 
-  def forget.user
+  def forget user
     update_attribute(:remember_digest)
 
   end
