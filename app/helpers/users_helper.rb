7333c7c8e35e335ee6e78336726cffa7b4c6e226
Puxton <clifford.morris.0844@gmail.com>
2022-04-14 04:11:37 -0400

    Remove unused function

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,13 +5,5 @@ module UsersHelper
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}"
     image_tag(gravatar_url, alt: user.name, class: "gravatar")
   end
-
-  def validated_signup_label(param, user, f)
-    if user.errors.full_messages_for(param).any?
-      f.label param, user.errors.full_messages_for(param).first, style:"color:red;"
-    else
-      f.label param
-    end     
-  end
-
 end
+
