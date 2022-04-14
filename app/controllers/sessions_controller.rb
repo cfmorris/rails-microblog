1c9b9bf077cc81c6eb8ad1e7dc856f1444a5799d
Puxton <clifford.morris.0844@gmail.com>
2022-04-14 17:36:23 -0400

    Update comments

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,7 +15,7 @@ class SessionsController < ApplicationController
   end
   
   def destroy
-    log_out
+    log_out #if logouts fail check here first and add `current user`
     redirect_to root_url
   end
 
