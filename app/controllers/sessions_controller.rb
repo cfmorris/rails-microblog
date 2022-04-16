15753d0c63c535b03a2d9f0d9dc47315938188b9
Puxton <clifford.morris.0844@gmail.com>
2022-04-16 16:03:04 -0400

    Implement form validation via locals

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,12 +10,12 @@ class SessionsController < ApplicationController
       redirect_to @user
     else
       flash.now[:danger] = 'Invalid email/password combination'
-      render 'new'
+      render 'new', status: :unprocessable_entity
     end
   end
   
   def destroy
-    log_out #if logouts fail check here first and add `current user`
+    log_out  
     redirect_to root_url
   end
 
