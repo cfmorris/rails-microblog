196c3ef78b03657109590867cb8384ffa939902e
Puxton <clifford.morris.0844@gmail.com>
2022-04-15 23:34:01 -0400

    Update HTTP method used for Sign up tests

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -23,7 +23,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
-    get logout_path
+    delete logout_path
     follow_redirect!
     assert_not is_logged_in?
   end
