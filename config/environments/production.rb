108c4cd2d274131491ffe13bcd9ddcbfde0ef8be
Puxton <clifford.morris.0844@gmail.com>
2022-04-19 16:28:27 -0400

    Update SMTP information

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,11 +72,11 @@ Rails.application.configure do
   host = 'frozen-beach-47064.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :addresses      => 'smtp.sendgrig.net',
+    :addresses      => 'smtp.gmail.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => ENV['GMAIL_USERNAME'],
+    :password       => ENV['GMAIL_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttles_auto => true
   }
