2cf2b6fc001f3fd9cfc72ffe56d7a4f689823567
Puxton <clifford.morris.0844@gmail.com>
2022-04-21 19:00:51 -0400

    Add activation account

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,14 +69,14 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'frozen-beach-47064.herokuapp.com'
+  host = 'https://git.heroku.com/frozen-beach-47064.git'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
     :addresses      => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'apikey',
+    :password       => ENV['SENDGRID_API_KEY'],
     :domain        => 'heroku.dev',
     :enable_starttls_auto => true
   }
