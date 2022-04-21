9d3e487ff0c3a360ef4976a8937d323d04c0ae4e
Puxton <clifford.morris.0844@gmail.com>
2022-04-21 14:42:43 -0400

    Fix mailer domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,14 +70,14 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   host = 'frozen-beach-47064.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: 'cfmorris.dev' }
   ActionMailer::Base.smtp_settings = {
     :addresses      => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
     :user_name      => ENV['SENDGRID_USERNAME'],
     :password       => ENV['SENDGRID_PASSWORD'],
-    :domain        => 'cfmorris.dev',
+    :domain        => 'heroku.com',
     :enable_starttls_auto => true
   }
 
