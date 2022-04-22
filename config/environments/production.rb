96edfeb38b903664c084b2bc2d745f3f9bd34b33
Puxton <clifford.morris.0844@gmail.com>
2022-04-22 10:25:55 -0400

    Troubleshoot mailer: URL link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -70,7 +70,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://git.heroku.com/frozen-beach-47064.git'
+  host = 'https://frozen-beach-47064.herokuapp.com/'
   config.action_mailer.default_url_options = { host: host }
   config.action_mailer.smtp_settings = {
     :address      => 'smtp.sendgrid.net',
